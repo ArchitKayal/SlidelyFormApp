5e908f8496dc07b4dfced306931a8e6caed2b48b
Archit-Kayal <architkayal@gmail.com>
2024-06-19 08:43:02 +0530

    created createsubmission form

--- SlidelyFormApp/CreateSubmissionForm.Designer.vb
+++ SlidelyFormApp/CreateSubmissionForm.Designer.vb
@@ -23,6 +23,17 @@ Partial Class CreateSubmissionForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        Button1 = New Button()
+        TextBox5 = New TextBox()
+        Button2 = New Button()
         SuspendLayout()
         ' 
         ' Label1
@@ -34,11 +45,111 @@ Partial Class CreateSubmissionForm
         Label1.TabIndex = 0
         Label1.Text = "Archit Kayal, Slidely Task 2 - Create Submission"
         ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(202, 98)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(59, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Name"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(202, 146)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(54, 25)
+        Label3.TabIndex = 2
+        Label3.Text = "Email"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(202, 195)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(106, 25)
+        Label4.TabIndex = 3
+        Label4.Text = "Phone Num"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(202, 245)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(185, 25)
+        Label5.TabIndex = 4
+        Label5.Text = "Github Link For Task 2"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(433, 92)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(150, 31)
+        TextBox1.TabIndex = 5
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(433, 140)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 6
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(433, 189)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(150, 31)
+        TextBox3.TabIndex = 7
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(433, 239)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(150, 31)
+        TextBox4.TabIndex = 8
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(202, 294)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(247, 34)
+        Button1.TabIndex = 9
+        Button1.Text = "Toggle Stopwatch (Ctrl + T)"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(461, 297)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(122, 31)
+        TextBox5.TabIndex = 10
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(202, 353)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(381, 34)
+        Button2.TabIndex = 11
+        Button2.Text = "Submit (Ctrl + S)"
+        Button2.UseVisualStyleBackColor = True
+        ' 
         ' CreateSubmissionForm
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(TextBox5)
+        Controls.Add(Button1)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         Name = "CreateSubmissionForm"
         Text = "CreateSubmissionForm"
@@ -47,4 +158,15 @@ Partial Class CreateSubmissionForm
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button2 As Button
 End Class
