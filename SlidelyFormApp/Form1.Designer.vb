b7e65a4b9a1bee6464fe9fb1b985384f300b2769
Archit-Kayal <architkayal@gmail.com>
2024-06-19 07:53:08 +0530

    Add project files.

---
+++ SlidelyFormApp/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        ButtonViewSubmissions = New Button()
+        ButtonNewSubmissions = New Button()
+        SuspendLayout()
+        ' 
+        ' ButtonViewSubmissions
+        ' 
+        ButtonViewSubmissions.Location = New Point(254, 184)
+        ButtonViewSubmissions.Name = "ButtonViewSubmissions"
+        ButtonViewSubmissions.Size = New Size(303, 34)
+        ButtonViewSubmissions.TabIndex = 0
+        ButtonViewSubmissions.Text = "View Submissions (Ctrl + V)"
+        ButtonViewSubmissions.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonNewSubmissions
+        ' 
+        ButtonNewSubmissions.Location = New Point(254, 242)
+        ButtonNewSubmissions.Name = "ButtonNewSubmissions"
+        ButtonNewSubmissions.Size = New Size(303, 34)
+        ButtonNewSubmissions.TabIndex = 1
+        ButtonNewSubmissions.Text = "Create New Submission (Ctrl + N)"
+        ButtonNewSubmissions.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(ButtonNewSubmissions)
+        Controls.Add(ButtonViewSubmissions)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents ButtonViewSubmissions As Button
+    Friend WithEvents ButtonNewSubmissions As Button
+
+End Class
---
+++ SlidelyFormApp/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+        Me.KeyPreview = True
+    End Sub
+
+    Private Sub Form1_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles MyBase.PreviewKeyDown
+        If e.Control AndAlso e.KeyCode = Keys.V Then
+            ButtonViewSubmissions.PerformClick()
+        ElseIf e.Control AndAlso e.KeyCode = Keys.N Then
+            ButtonNewSubmissions.PerformClick()
+        End If
+    End Sub
+End Class
